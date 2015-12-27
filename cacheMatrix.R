44cb51149025d3aa473be598bb1018120564f654
staszek-krotki <stasz@interia.pl>
2015-12-27 21:14:46 +0100

    implementation of makeCacheMatrix

--- cacheMatrix.R
+++ cacheMatrix.R
@@ -1,5 +1,32 @@
+# This is a function that returns a special cache matrix object which is a list of methods to operate on it:
+#   set() - sets the matrix
+#   get() - gets the matrix
+#   setInversedMatrix() - sets the inversed matrix
+#   getInversedMatrix() - gets the inversed matrix
+
 makeCacheMatrix <- function(x = matrix()) {
+    inversed_matrix <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        inversed_matrix <<- NULL
+    }
+    
+    get <- function() {
+        x
+    }
+    
+    setInversedMatrix <- function(m) {
+        inversed_matrix <<- m
+    } 
+    
+    getInversedMatrix <- function() {
+        inversed_matrix
+    }
+    
+    list(set = set, get = get, setInversedMatrix = setInversedMatrix, getInversedMatrix = getInversedMatrix)
 }
 
 cacheSolve <- function(x, ...) {
+    
 }
