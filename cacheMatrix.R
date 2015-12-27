50d52eaa524e6c8877743d916851f57132b7c0b6
staszek-krotki <stasz@interia.pl>
2015-12-27 21:19:53 +0100

    implementation of cacheSolve

--- cacheMatrix.R
+++ cacheMatrix.R
@@ -27,6 +27,20 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get, setInversedMatrix = setInversedMatrix, getInversedMatrix = getInversedMatrix)
 }
 
+# This function solves a matrix. It can only operate on the object returned by makeCacheMatrix
+# if the matrix has already been solved then it is returned form the cache
+
 cacheSolve <- function(x, ...) {
+    m <- x$getInversedMatrix()
     
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInversedMatrix(m)
+    
+    m
 }
